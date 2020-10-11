8976875a6c967ea616ddb5393cb84b4f02378108
wanlop-357 <wanlop.aot.art@gmail.com>
2020-10-11 20:00:48 +0700

    commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use App\Models\DBModel;
 class HomeController extends Controller
 {
     public function test(){
@@ -18,10 +19,11 @@ class HomeController extends Controller
     public function index(){
         echo view('Home');
     }
+    
     public function getdata(){
-        $data = DB::select("SELECT * FROM `regiter_account`");
-        $data = json_decode(json_encode($data), true);
-        return json_encode($data) ;
+        $BDM = new DBModel();
+        $data = $BDM->getdata();
+        return $data;
     }
 
     
--- app/models/DBModel.php
+++ app/models/DBModel.php
@@ -7,13 +7,7 @@ use Illuminate\Support\Facades\DB;
 
 class DBModel extends Model{
     public function getdata(){
-        // $data = array(1,2,3);
-        // return $data;
-        // // $user = DB::table('users')->find(3);
-        // $users = DB::table('regiter_account')->select('username')->get();
-        // // $db = DB::table('regiter_account');
-        // return $users;
-        // print_r($db);
-        // exit();
+        $data = DB::select("SELECT * FROM `regiter_account`");
+        return $data ;
     }
 }
